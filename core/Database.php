e76992e0ce914b9fc6d179005e6f33dcb84eef7d
pmalicki11 <pmalicki11@gmail.com>
2020-06-23 21:47:56 +0200

    Moved db connection params to config file

--- core/Database.php
+++ core/Database.php
@@ -8,7 +8,7 @@
 
     private function __construct() {
       try {
-        $this->pdo = new PDO('mysql:host=127.0.0.1;dbname=invoices', 'root', 'root');
+        $this->pdo = new PDO('mysql:host=' . DBHOST . ';dbname=' . DBNAME, DBUSER, DBPASSWORD);
       } catch (PDOException $e) {
         die($e->getMessage());
       }
